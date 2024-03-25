f251dd0b45c8d7aa5702037adde1f8005e37730b
VictorBarrozo <victorbarrozoads@gmail.com>
2024-03-24 22:29:28 -0300

    interface de login

--- app/src/main/java/com/victorbarrozo/whatsappfirebase/CadastroActivity.kt
+++ app/src/main/java/com/victorbarrozo/whatsappfirebase/CadastroActivity.kt
@@ -5,18 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.victorbarrozo.whatsappfirebase.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
+class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_cadastro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
