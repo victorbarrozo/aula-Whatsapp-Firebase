12fbb853391d6a53ae4058e3f051ca782b42cad2
VictorBarrozo <victorbarrozoads@gmail.com>
2024-03-26 22:15:07 -0300

    funcao deslogar menu customizado

--- app/src/main/java/com/victorbarrozo/whatsappfirebase/LoginActivity.kt
+++ app/src/main/java/com/victorbarrozo/whatsappfirebase/LoginActivity.kt
@@ -32,14 +32,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var senha: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      //  enableEdgeToEdge()
-        setContentView(binding.root)
-       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
 
+        setContentView(binding.root)
         inicializarEventosClique()
         firebaseAuth.signOut()
     }
--- app/src/main/java/com/victorbarrozo/whatsappfirebase/MainActivity.kt
+++ app/src/main/java/com/victorbarrozo/whatsappfirebase/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.victorbarrozo.whatsappfirebase
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.victorbarrozo.whatsappfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +20,58 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        inicializarToobar()
+
+    }
+
+    private fun deslogarUsuario() {
+        AlertDialog.Builder(this)
+            .setTitle("Deslogar")
+            .setMessage("Realmente deseja sair?")
+            .setNegativeButton("Não") { dialog, posicao -> }
+            .setPositiveButton("sim") { dialog, posicao ->
+                firebaseAuth.signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }.create().show()
+    }
+
+    private fun inicializarToobar() {
+        val toolbar = binding.tbMain.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "WhatsApp"
         }
+        addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_principal, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    when (menuItem.itemId) {
+                        R.id.itemPerfil -> {
+                            startActivity(Intent(applicationContext, PerfilActivity::class.java))
+                        }
+
+                        R.id.itemSair -> {
+                            deslogarUsuario()
+                        }
+                    }
+                    return true
+                }
+
+            }
+        )
     }
 }
